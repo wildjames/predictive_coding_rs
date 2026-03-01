2179e72b30d1b299e0ad6d8885879b8d7ebe68be
Jim Wild <wild.james343@gmail.com>
2026-03-01 21:03:39 +0000

    Reduce code duplication

--- src/train_model_handler.rs
+++ src/train_model_handler.rs
@@ -15,19 +15,38 @@ use liveplot::{
   run_liveplot
 };
 
-
-/// Run inference to convergence on a single sample and update weights.
-fn converge_sample(
+fn set_input_and_output(
   model: &mut model::PredictiveCodingModel,
-  input_values: Array1<f32>,
-  output_values: Array1<f32>,
-  convergence_threshold: f32,
-  convergence_steps: u32
+  data: &train_data_handler::ImagesBWDataset
 ) {
+  let rand_index = usize::from_ne_bytes(rand::random()) % data.num_images;
+
+  // Normalise to the range 0..1
+  let input_values: Array1<f32> = data.images
+    .row(rand_index)
+    .mapv(|x| x as f32 / 255.0)
+    .to_owned();
+
+  // One-hot output row with label value set to 1.0
+  let output_label: usize = data.labels[rand_index] as usize;
+
+  let output_layer_size = model.layers.last().unwrap().size;
+  let mut output_values: Array1<f32> = Array1::zeros(output_layer_size);
+  output_values[output_label] = 1.0;
 
   model.set_input(input_values);
   model.set_output(output_values);
 
+}
+
+/// Run inference to convergence on a single sample and update weights.
+fn converge_sample(
+  model: &mut model::PredictiveCodingModel,
+  data: &train_data_handler::ImagesBWDataset,
+  convergence_threshold: f32,
+  convergence_steps: u32
+) {
+  set_input_and_output(model, data);
   // Train on this example until convergence.
   model.converge_values(convergence_threshold, convergence_steps);
   model.compute_predictions_and_errors();
@@ -44,27 +63,11 @@ pub fn train(
   convergence_threshold: f32
 ) {
 
-  let output_layer_size = model.layers.last().unwrap().size;
 
   for step in 0..training_steps {
-    let rand_index = usize::from_ne_bytes(rand::random()) % data.num_images;
-
-    // Normalise to the range 0..1
-    let input_values: Array1<f32> = data.images
-      .row(rand_index)
-      .mapv(|x| x as f32 / 255.0)
-      .to_owned();
-
-    // One-hot output row with label value set to 1.0
-    let output_label: usize = data.labels[rand_index] as usize;
-    debug!("Training on sample {}, with label {}", rand_index, output_label);
-    let mut output_values: Array1<f32> = Array1::zeros(output_layer_size);
-    output_values[output_label] = 1.0;
-
     converge_sample(
       model,
-      input_values,
-      output_values,
+      data,
       convergence_threshold,
       convergence_steps
     );
@@ -104,27 +107,10 @@ pub fn train_plotting_local(
   // Run the dataset within a worker thread, since the plotter wants the main one.
   std::thread::scope(|s| {
     s.spawn(move || {
-      let output_layer_size = model.layers.last().unwrap().size;
-
       for step in 0..training_steps {
-        let rand_index = usize::from_ne_bytes(rand::random()) % data.num_images;
-
-        // Normalise to the range 0..1
-        let input_values: Array1<f32> = data.images
-          .row(rand_index)
-          .mapv(|x| x as f32 / 255.0)
-          .to_owned();
-
-        // One-hot output row with label value set to 1.0
-        let output_label: usize = data.labels[rand_index] as usize;
-        debug!("Training on sample {}, with label {}", rand_index, output_label);
-        let mut output_values: Array1<f32> = Array1::zeros(output_layer_size);
-        output_values[output_label] = 1.0;
-
         converge_sample(
           model,
-          input_values,
-          output_values,
+          data,
           convergence_threshold,
           convergence_steps
         );
