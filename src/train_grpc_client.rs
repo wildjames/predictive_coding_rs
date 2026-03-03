352d9de1ae1c876d01d21a0dd0418e109c4b65de
Jim Wild <james.wild6@nhs.net>
2026-03-03 00:02:07 +0000

    Fix devcontainer

--- src/train_grpc_client.rs
+++ src/train_grpc_client.rs
@@ -5,6 +5,9 @@ use tonic::transport::Endpoint;
 async fn main() {
   utils::setup_tracing();
 
-  let addr: Endpoint = "http://127.0.0.1:50051".parse().unwrap();
+  let addr: Endpoint = std::env::var("SERVER_ADDR")
+    .unwrap_or("http://192.168.3.111:50051".into())
+    .parse()
+    .unwrap();
   train_model_handler::start_grpc_client(addr).await.unwrap();
 }
